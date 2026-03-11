5a69aca040b0b96df109117e2279e84d7139582b
harip <harip@Hariprasad>
2026-03-12 00:53:10 +0530

    Added while loop program

---
+++ src/Whileloop.java
@@ -0,0 +1,13 @@
+
+public class Whileloop {
+
+	public static void main(String[] args) {
+		int count = 1;
+		while (count <= 5) {
+			System.out.println("count: " + count);
+			count++;
+			
+		}
+	}
+
+}
