f9b9f1809f118c70741c7a7e99a331c7ceb1d397
harip <harip@Hariprasad>
2026-03-13 18:54:12 +0530

    Initial commit of BaseDemo

---
+++ src/packages/BaseDemo.java
@@ -0,0 +1,15 @@
+package packages;
+
+public class BaseDemo {
+
+	public static void main(String[] args) {
+		Base ob = new Base();
+		ob.setEid(101);
+		ob.setEname("Hello");
+		System.out.println(ob.getEid());
+		System.out.println(ob.getEname());
+		
+		
+	}
+
+}
