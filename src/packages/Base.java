a0edc3f566041feb5db309babb33d65d4e14de31
harip <harip@Hariprasad>
2026-03-14 14:07:08 +0530

    added packages

--- src/packages/Base.java
+++ src/packages/Base.java
@@ -1,34 +1,31 @@
 package packages;
 
 public class Base {
+	public static void main (String[] args) {
+		Base b = new Base();
+		b.setEid(101);
+		b.setEname("Hari");
+		System.out.println(b.getEid());
+		System.out.println(b.getEname());
+		
+	}
+	        private int eid;
+	        private String ename;
+	   
 
-    private int eid;
-    private String ename;
+	        public int getEid() {
+	            return eid;
+	        }
 
-    public int getEid() {
-        return eid;
-    }
+	        public void setEid(int eid) {
+	            this.eid = eid;
+	        }
 
-    public void setEid(int eid) {
-        this.eid = eid;
-    }
+	        public String getEname() {
+	            return ename;
+	        }
 
-    public String getEname() {
-        return ename;
-    }
-
-    public void setEname(String ename) {
-        this.ename = ename;
-    }
-
-    public static void main(String[] args) {
-
-        Base obj = new Base();
-
-        obj.setEid(101);
-        obj.setEname("Hariprasad");
-
-        System.out.println("Employee ID: " + obj.getEid());
-        System.out.println("Employee Name: " + obj.getEname());
-    }
-}
+	        public void setEname(String ename) {
+	            this.ename = ename;
+	        }
+	    }
