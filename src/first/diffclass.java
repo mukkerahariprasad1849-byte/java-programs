7acf7233521bcb585f72449e6dd3ca034603a637
harip <harip@Hariprasad>
2026-03-14 21:36:07 +0530

    Added Java example for access modifiers using two classes

---
+++ src/first/diffclass.java
@@ -0,0 +1,23 @@
+package first;
+
+public class diffclass {
+	public static void main(String args[])
+	{
+		sameClass sc = new sameClass();
+		sc.defaultmethod();
+		sc.privatemethod();
+		sc.protmethod();
+		sc.publicmethod();
+		
+		//private member can't accessible
+		//System.out.println(sc.privari);
+		
+		System.out.println(sc.defvari);
+		System.out.println(sc.protvari);
+		System.out.println(sc.pubvari);
+		
+	}
+	
+}
+
+
