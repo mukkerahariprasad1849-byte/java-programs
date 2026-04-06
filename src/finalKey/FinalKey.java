69e8e9df4f5e8ca8b20e5d9c0d8b89bbb00f3448
harip <harip@Hariprasad>
2026-04-07 00:10:00 +0530

    inserted finalkey

---
+++ src/finalKey/FinalKey.java
@@ -0,0 +1,14 @@
+package finalKey;
+
+public class FinalKey {
+	
+	    public static void main(String[] args) {
+	        final int x = 10;
+
+	        // x = 20; ❌ Error (final variable)
+
+	        System.out.println("Value of x: " + x);
+	    }
+	}
+
+
